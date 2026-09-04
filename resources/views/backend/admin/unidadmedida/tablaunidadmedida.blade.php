98c9968b5e7fd26b2a59592397908441f5538baf
Tatan <tatangamess@gmail.com>
2026-09-04 12:27:03 -0600

    ajustes

--- resources/views/backend/admin/unidadmedida/tablaunidadmedida.blade.php
+++ resources/views/backend/admin/unidadmedida/tablaunidadmedida.blade.php
@@ -7,6 +7,7 @@
                         <table id="tabla" class="table table-bordered table-striped">
                             <thead>
                             <tr>
+                                <th style="width: 4%">ID</th>
                                 <th style="width: 80%">Medida</th>
                                 <th style="width: 10%">Opciones</th>
                             </tr>
@@ -15,6 +16,7 @@
 
                             @foreach($lista as $dato)
                                 <tr>
+                                    <td>{{ $dato->id }}</td>
                                     <td>{{ $dato->nombre }}</td>
                                     <td>
                                         <button type="button" class="btn btn-primary btn-xs" onclick="informacion({{ $dato->id }})">
